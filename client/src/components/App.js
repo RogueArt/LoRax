c5d6647f277a4f51460bf2f502e9fb458ee521e7
RogueArt <altoraries@gmail.com>
2022-01-14 21:58:34 -0800

    Install sass and a node rendering library, add node rendering starter code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import './App.css';
+import ReactFlow from 'react-flow-renderer';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Hello from React</h1>
-    </div>
-  );
+const elements = [
+  { id: '1', data: { label: 'Node 1' }, position: { x: 250, y: 5 } },
+  // you can also pass a React component as a label
+  { id: '2', data: { label: <div>Node 2</div> }, position: { x: 100, y: 100 } },
+  { id: 'e1-2', source: '1', target: '2', animated: true },
+];
+
+function createNodeConnection(sourceID, targetID) {
+  return {
+    id: `e${sourceID}-${targetID}`,
+    source: sourceID,
+    target: targetID,
+    animated: true,
+  }
 }
 
-export default App;
+const BasicFlow = () => (
+  <div style={{ width: 1600, height: 1600 }}>
+    <ReactFlow elements={elements} />
+    </div>);
+
+export default BasicFlow
