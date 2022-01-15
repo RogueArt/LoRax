33ea5ff7669b60c44799730d6e29a0e0350aa525
8BitRobot <premgiridhar11@gmail.com>
2022-01-15 14:48:13 -0800

    Added some basic React websocket stuff.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,27 +4,72 @@ import '../styles/App.scss';
 // import ReactFlow from 'react-flow-renderer';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      soil: {
+        value: "-",
+        warning: "",
+      },
+      temp: {
+        value: "-",
+        warning: "",
+      },
+      humid: {
+        value: "-",
+        warning: "",
+      },
+      uv: {
+        value: "-",
+        warning: "",
+      },
+    }
+  }
+
+  componentDidMount () {
+    this.connection = new WebSocket("ws://localhost:8081");
+    this.connection.onopen = () => {
+      this.connection.send(JSON.stringify(
+        {
+          "type": 0, // registering
+          "from": "client",
+        }
+      ));
+    }
+    this.connection.onmessage = (msg) => {
+      msg = JSON.parse(msg);
+      if (msg.type === 1) {
+        let obj = {};
+        obj[msg.sensor] = msg.value;
+        this.setState(obj);
+      }
+    }
+  }
+
   render () {
     return (
       <main>
         <div className="info-cards">
           <InfoCard
             title="Soil Moisture"
-            value="100%"
-            warning="This soil is highly unsafe."
+            value={this.state.soil.value}
+            warning={this.state.soil.warning}
           />
           <InfoCard
             title="Temperature"
-            value="67°F"
+            value={this.state.temp.value}
+            warning={this.state.temp.warning}
           />
           <InfoCard
             title="UV Light"
-            value="Normal"
+            value={this.state.uv.value}
+            warning={this.state.uv.warning}
             smaller
           />
           <InfoCard
             title="Humidity"
-            value="53%"
+            value={this.state.humid.value}
+            warning={this.state.humid.warning}
           />
         </div>
         <div className="map-container"></div>
