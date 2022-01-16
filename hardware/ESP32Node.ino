4c629fb4762340ddf6275babf8e013704fdc0a03
SiddhantG02 <SiddhantG.me@gmail.com>
2022-01-15 16:38:23 -0800

    Added websocket client functionality to esp32
    Combined the code for the ESP32 Node for the sensor data extraction with the websocket client code

--- hardware/ESP32Node.ino
+++ hardware/ESP32Node.ino
@@ -1,11 +1,15 @@
-
 #include <Wire.h>
 #include "Adafruit_VEML6070.h"
 #include "Adafruit_Si7021.h"
 #include "Adafruit_seesaw.h"
 #include <TinyGPS++.h>
 #include <HardwareSerial.h>
-
+#include <ArduinoWebsockets.h>
+#include <ArduinoJson.h>
+#include <WiFi.h>
+#include "secrets.h" //has defines for wifi ssid, password, server info
+using namespace websockets;
+WebsocketsClient client;
 float latitude , longitude;
 String  lat_str , lng_str;
 Adafruit_seesaw ss;
@@ -17,10 +21,38 @@ int incomingByte = 0;
 void setup()
 {
   
-Serial.begin(9600);
-SerialGPS.begin(9600, SERIAL_8N1, 16, 17);
-Serial.println("VEML6070 Test");
-uv.begin(VEML6070_1_T); // pass in the integration time constant
+  Serial.begin(115200);
+  SerialGPS.begin(115200, SERIAL_8N1, 16, 17);
+  WiFi.begin(ssid, password);
+
+    // Wait some time to connect to wifi
+    for(int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++) {
+        Serial.print(".");
+        delay(1000);
+    }
+
+    // Check if connected to wifi
+    if(WiFi.status() != WL_CONNECTED) {
+        Serial.println("No Wifi!");
+        return;
+    }
+
+    Serial.println("Connected to Wifi, Connecting to server.");
+    // try to connect to Websockets server
+    bool connected = client.connect(websockets_server_host);
+    if(connected) {
+        Serial.println("Connected!");
+        client.send("Hello Server");
+    } else {
+        Serial.println("Not Connected!");
+    }
+    
+    // run callback when messages are received
+    client.onMessage([&](WebsocketsMessage message){
+        Serial.print("Got Message: ");
+        Serial.println(message.data());
+    });
+  uv.begin(VEML6070_1_T); // pass in the integration time constant
   if (!ss.begin(0x36)) {
     Serial.println("ERROR! seesaw not found");
     while(1) delay(1);
@@ -29,20 +61,37 @@ uv.begin(VEML6070_1_T); // pass in the integration time constant
     Serial.println(ss.getVersion(), HEX);
   }
 }
- 
+
+unsigned long messageTimestamp = 0;
+
 void loop()
 {
-Serial.print("UV light level: ");
-Serial.println(uv.readUV());
-Serial.print("Humidity:    ");
-Serial.print(sensor.readHumidity(), 2);
-Serial.print("\tTemperature: ");
-Serial.println(sensor.readTemperature(), 2);
-float tempC = ss.getTemp();
-uint16_t capread = ss.touchRead(0);
+  // let the websockets client check for incoming messages
+  if(client.available()) {
+     client.poll();
+        
+    uint64_t now = millis();
+    if (now - messageTimestamp > 5000) 
+    {
+      messageTimestamp = now;
+  
+      client.send("hullo");
+  
+      // Print JSON for debugging
+      //Serial.println(output);
+    }
+  }
+  Serial.print("UV light level: ");
+  Serial.println(uv.readUV());
+  Serial.print("Humidity:    ");
+  Serial.print(sensor.readHumidity(), 2);
+  Serial.print("\tTemperature: ");
+  Serial.println(sensor.readTemperature(), 2);
+  float tempC = ss.getTemp();
+  uint16_t capread = ss.touchRead(0);
 
-Serial.print("Ground Temperature: "); Serial.print(tempC); Serial.println("*C");
-Serial.print("Ground Capacitive: "); Serial.println(capread);
+  Serial.print("Ground Temperature: "); Serial.print(tempC); Serial.println("*C");
+  Serial.print("Ground Capacitive: "); Serial.println(capread);
 
   if (SerialGPS.available() > 0) {
     // read the incoming byte:
@@ -51,8 +100,7 @@ Serial.print("Ground Capacitive: "); Serial.println(capread);
     // say what you got:
     Serial.print("I received: ");
     Serial.println(incomingByte, DEC);
-  } else {Serial.println("Just rage quit");}
+  } 
 
-
-delay(1000);
+  delay(1000);
 }
