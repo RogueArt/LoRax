acf35dcf1890c89a163004b3122df13030953e7c
RogueArt <altoraries@gmail.com>
2022-01-16 04:26:29 -0800

    Wrap app state with context

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../styles/Header.scss';
 import PropTypes from 'prop-types';
+import Arrow from '../styles/imports/arrow.svg'
+import { Link } from "react-router-dom";
 
-function Header({ title = 'Backyard Kipling', signalStatus = '' }) {
+function Header({ title = 'Backyard Kipling', signalStatus = '', disableBackButton=false }) {
     return (
         <header>
-            <div>
-            </div>
+            <Link to="/">
+                <div className="back-button" style={disableBackButton ? { display: 'none' } : {}}>
+                    <img className="arrow" src={Arrow}/>
+                </div>
+            </Link>
             <div className="header-text">
                 <h1 style={{ margin: 0 }}>{title}</h1>
                 {signalStatus ? <h6>Signal Strength: <strong>{signalStatus}</strong></h6> : null}                
